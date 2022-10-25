048dad6c6d69cf2b563b62d3238f3181f6a612d7
Bryan Parada <bryan.parada.cea@gmail.com>
2022-10-25 14:18:25 -0300

    V261 - Reactive: Update Person values

--- src/app/reactive/switches/switches.component.ts
+++ src/app/reactive/switches/switches.component.ts
@@ -29,6 +29,40 @@ export class SwitchesComponent implements OnInit{
       terms: false
       }
        );
+
+    //Si queremos que las propiedades en Person esten sincronizados es necesario suscribirnos 
+    //a los cambios del formulario 
+    ///------OPCION 1
+    // this.myFormSwitches.valueChanges.subscribe( form => {
+    //   // console.log(form);
+    //   delete form.terms
+    //   this.person = form;
+      
+    // }); 
+
+    ///------OPCION 2
+    this.myFormSwitches.valueChanges.subscribe( ({terms, ...restOfArguments}) => {
+      // console.log(form); 
+      this.person = restOfArguments;
+      
+    });
+
+    //Suscribirse a algun campo en particular
+    // this.myFormSwitches.get('terms')?.valueChanges.subscribe( newValue => {
+    //   console.log(newValue);
+      
+    // });
+
+ 
+  }
+
+  save(){
+    const formValue = {...this.myFormSwitches.value};
+    delete formValue.terms
+    // console.log(formValue);
+    
+    this.person = formValue;
+  
   }
   
 }
