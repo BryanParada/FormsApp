986b780f42cf53f5706ad8a75bcc9b99e9f90a79
Bryan Parada <bryan.parada.cea@gmail.com>
2022-10-25 10:48:01 -0300

    V252 - Reactive: FormBuilder

--- src/app/reactive/basics/basics.component.ts
+++ src/app/reactive/basics/basics.component.ts
@@ -1,5 +1,5 @@
 import { Component} from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-basics',
@@ -9,11 +9,19 @@ import { FormGroup, FormControl } from '@angular/forms';
 })
 export class BasicsComponent{
 
-  myForm: FormGroup = new FormGroup({
-    'name': new FormControl('RTX 4080ti')
+  // myForm: FormGroup = new FormGroup({
+  //   'name'  : new FormControl('RTX 4080ti'),
+  //   'price' : new FormControl(1500),
+  //   'stock' : new FormControl(5)
+  // })
+
+  myForm: FormGroup = this.fb.group({
+    name : ['RTX 4080ti'],
+    price: [1500],
+    stock: [5],
   })
 
-  constructor() { }
+  constructor( private fb: FormBuilder) { }
  
 
 }
