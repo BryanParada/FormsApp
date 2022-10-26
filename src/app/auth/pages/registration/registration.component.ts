9fd5a75546e751c6fbc0cdafdf48fc0b854afc75
Bryan Parada <bryan.parada.cea@gmail.com>
2022-10-26 11:06:04 -0300

    V269 - RegEx name

--- src/app/auth/pages/registration/registration.component.ts
+++ src/app/auth/pages/registration/registration.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registration',
@@ -8,9 +9,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrationComponent implements OnInit {
 
-  constructor() { }
+  //TODO: TEMPORAL
+  name_lastnamePattern: string = '([a-zA-Z]+) ([a-zA-Z]+)';
+  // '([a-zA-Z]+) (.+[a-zA-Z]+).+$'; // para 2 apellidos Te obliga a escribir al menos 2 palabras, pero te permite añadir 3 o más.
+
+  myFormRegistration: FormGroup = this.fb.group({
+    name: ['', [Validators.required, Validators.pattern( this.name_lastnamePattern)] ]
+  })
+
+  constructor( private fb: FormBuilder) { }
 
   ngOnInit(): void {
+  }
+
+  invalidField(field: string){
+    return this.myFormRegistration.get(field)?.invalid
+          && this.myFormRegistration.get(field)?.touched
+  }
+
+  submitForm(){
+    console.log(this.myFormRegistration.value);
+    this.myFormRegistration.markAllAsTouched;
+    
   }
 
 }
