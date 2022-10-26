93707ad375b5af1caed73b01a759569e5c43cd36
Bryan Parada <bryan.parada.cea@gmail.com>
2022-10-26 11:17:00 -0300

    V270 - Email RegEx

--- src/app/auth/pages/registration/registration.component.ts
+++ src/app/auth/pages/registration/registration.component.ts
@@ -11,15 +11,21 @@ export class RegistrationComponent implements OnInit {
 
   //TODO: TEMPORAL
   name_lastnamePattern: string = '([a-zA-Z]+) ([a-zA-Z]+)';
+  emailPattern: string = "^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$";
   // '([a-zA-Z]+) (.+[a-zA-Z]+).+$'; // para 2 apellidos Te obliga a escribir al menos 2 palabras, pero te permite añadir 3 o más.
 
   myFormRegistration: FormGroup = this.fb.group({
-    name: ['', [Validators.required, Validators.pattern( this.name_lastnamePattern)] ]
+    name : ['', [Validators.required, Validators.pattern( this.name_lastnamePattern)] ],
+    email: ['', [Validators.required, Validators.pattern( this.emailPattern ) ] ]
   })
 
   constructor( private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.myFormRegistration.reset({
+      name: 'Bryan Parada',
+      email: 'bryan@gmail.com'
+    })
   }
 
   invalidField(field: string){
